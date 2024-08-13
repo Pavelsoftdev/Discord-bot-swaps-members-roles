f2d9df9e7477e1aa4cf1442e92a6e3080317946a
Pavelsoftdev <126071902+Pavelsoftdev@users.noreply.github.com>
2024-08-13 23:13:22 +0300

    Update botrun.py

--- botrun.py
+++ botrun.py
@@ -1,3 +1,4 @@
+from config import *
 import discord
 from discord.ext import commands
 import os
@@ -8,22 +9,23 @@ intents.message_content = True
 intents.members = True 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+async def check_role_on_server(ctx, rol):
+     guild = bot.get_guild(SERVER_ID)  
+     role = get(guild.roles, name = rol)
+     if role is None:
+        await ctx.send(f"Помилка! Ha сервері немає ролі - {rol}")
+        return
 
-SERVER_ID = 1269678241417924809
-NAME_ROLE1 = "2 курс"
-NAME_ROLE2 = "3 курс"
-KICK_ROLE = "4 курс"
+
 @bot.command()
 async def stack(ctx):
     guild = bot.get_guild(SERVER_ID)  
-
     role1 = get(guild.roles, name=NAME_ROLE1)
     role2 = get(guild.roles, name=NAME_ROLE2)
     
-    if role1 is None or role2 is None:
-        await ctx.send(f"Ha сервері немає a6o ролі {NAME_ROLE1} a6o {NAME_ROLE2}")
-        return
-    
+    await check_role_on_server(ctx, NAME_ROLE1)
+    await check_role_on_server(ctx, NAME_ROLE2)
+
     members = role1.members
 
     for member in members:
@@ -34,12 +36,9 @@ async def stack(ctx):
 @bot.command()
 async def kickk(ctx):
     guild = bot.get_guild(SERVER_ID)  
-
     role = get(guild.roles, name = KICK_ROLE)
     
-    if role is None:
-        await ctx.send(f"Ha cepвepi немає poлi {KICK_ROLE}")
-        return
+    await check_role_on_server(ctx, KICK_ROLE)
 
     members = role.members
     for member in members:
